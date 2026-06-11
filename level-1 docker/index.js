ce829949e2fb4e031355fa2f98b0f291b1edc235
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-11 11:01:58 +0530

    day-1

---
+++ level-1 docker/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express()
+const  PORT = process.env.PORT || 5000
+
+
+
+app.use(express.json())
+
+app.get("/" ,(req,res) => {
+    res.send ( "hey this sever is running ")
+
+})
+
+app.listen(PORT,()=>{
+console.log(`server is running on port ${PORT} `)
+})
+
+
