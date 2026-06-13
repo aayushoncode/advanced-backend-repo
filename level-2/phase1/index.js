5593611f1840ba5c6f7e83b26d60f9dd300b97df
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-13 16:09:55 +0530

    day3

--- level-2/phase1/index.js
+++ level-2/phase1/index.js
@@ -1,20 +1,91 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+import dns from "dns";
+import User from "./models/user.js";
+import Redis from "ioredis";
+import { stringify } from "querystring";
 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-const app = express()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-app.use(express.json())
+const redis = new Redis(process.env.REDIS_URL);
 
-app.get("/",(req,res)=>{
-    res.send("this server is running ")
+app.use(express.json());
 
-})
+// Connect Database
+connectDB();
 
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "hello from redis",
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is runnig on port ${PORT}`)
-})
+app.post("/create", async (req, res) => {
+  const { name, email, password } = req.body;
+  await redis.del("user:all");
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  return res.json(user);
+}); 
+app.get("/get", async (req, res) => {
+  const user = await User.find({});
+  return res.json(user);
+});
 
+app.get("/get-with-redis", async (req, res) => {
+  const cached = await redis.get("user:all");
+
+  if (cached) {
+    const user = JSON.parse(cached);
+    return res.json(user);
+  }
+
+  const user = await User.find({});
+
+  await redis.set("user:all", JSON.stringify(user));
+
+  return res.json(user);
+});
+
+app.post("/send-otp", async (req, res) => {
+  const {email} = req.body;
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  await redis.set(`otp:${email}`, otp, "EX", 100);
+  return res.json({
+    otp,
+  });
+});
+
+app.post("/verify-otp", async (req, res) => {
+  const { email, otp } = req.body;
+  const cachedOtp = await redis.get(`otp:${email}`);
+
+  if (!cachedOtp) {
+    res.status(400).json({
+      message: "otp not found or expired",
+    });
+  }
+  if (cachedOtp != otp) {
+    res.status(400).json({
+      message: "otp incorrect",
+    });
+  }
+  return res.status(200).json({
+    message: "otp-verified",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
