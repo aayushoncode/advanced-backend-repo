5407fc8d77f273271161bb4841e2aa5e7a7ae9e9
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-24 12:03:36 +0530

    level-4 updates

--- level-4/index.js
+++ level-4/index.js
@@ -1,10 +1,55 @@
 import express from "express";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+import { ChatGroq } from "@langchain/groq";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
+
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+// });
+
+// app.post("/ai", async (req, res) => {
+//   const { input } = req.body;
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3.5-flash",
+//     contents: [
+//       {
+//         role: "system",
+//         parts: [{ text: "you are assistant and your name is jarvis " }],
+//       },
+
+//       {
+//         role: "user",
+//         parts: [{ text: input }],
+//       },
+//     ],
+//   });
+
+//   return res.status(200).json({
+//     " ai : ": response.text,
+//   });
+// });
+
+const llm = new ChatGroq({
+  model: "llama-3.3-70b-versatile",
+  temperature: 0,
+});
+
+app.post("/ai", async (req, res) => {
+  const { input } = req.body;
+
+  const response = await llm.invoke(input);
+
+  return res.status(200).json({
+    "ai : ": response.content,
+  });
+});
 
 app.get("/", (req, res) => {
   return res.status(200).json({
